071b0af201155244d6e45967dcc1f0869c572513
João Pedro <joao_pedro01@terra.com.br>
2024-04-16 19:09:47 -0300

    End project

--- app.js
+++ app.js
@@ -3,17 +3,6 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-/* 
-    GET - Listar todos os alunos (RA, Nome, Curso)
-    GET - Listar um alunoo através do RA informado (Nome, Turma, Cursos)
-    POST - Adicionar um aluno na lista
-    POST - Adicionar um curso para aluno
-    PUT - Alterar os dados de um aluno através do RA
-    PUT - Alterar o curso do aluno
-    DELETE - Remover aluno da lista
-    DELETE - Remover o curso do aluno
-*/
-
 var alunos = [
     {
         ra: 123,
@@ -30,21 +19,96 @@ var alunos = [
 ];
 
 app.get('/', (req, res) => {
-  res.json(alunos);
+    res.json(alunos);
+});
+
+app.get('/:ra', (req, res) => {
+    const ra = req.params.ra;
+    let aluno = alunos.find(x => x.ra == ra);
+
+    if (!aluno) {
+        return res.status(404).send('Aluno não encontrado');
+    }
+
+    res.json(aluno);
 });
 
 app.post('/', (req, res) => {
-  
+    let aluno = {
+        ra: req.body.ra,
+        nome: req.body.nome,
+        turma: req.body.turma,
+        habilidade: req.body.habilidades
+    };
+    alunos.push(aluno);
+
+    res.status(201).send('Aluno criado com sucesso');
 });
 
+app.post('/', (req, res) => {
+    let aluno = alunos.findIndex(x => x.ra == req.body.ra);
+    
+    if (aluno) {
+        return res.status(409).send('Aluno já cadastrado');
+    }
+
+    alunos.push(req.body);
+    res.status(201).send('Aluno criado com sucesso');
+});
+
+
 app.put('/', (req, res) => {
-  
+    let ra = req.query.ra;
+
+    index = alunos.findIndex(x => x.ra == ra);
+    alunos[index] = {
+        ra: req.body.ra,
+        nome: req.body.nome,
+        habilidades: req.body.habilidades
+    };
+
+    res.status(200).send('Aluno alterado com sucesso');
+});
+
+app.put('/:ra', (req, res) => {
+    let ra = req.params.ra;
+    let index = alunos.findIndex(x => x.ra == ra);
+    let aluno = alunos.find(x => x.ra == ra);
+
+    if (aluno) {
+        return res.status(404).send('Aluno não encontrado');
+    }
+
+    alunos[index]
+    aluno.turma = req.body.turma;
+    alunos[index] = aluno;
+    res.status(200).send('Turma alterada com sucesso');
+    
 });
 
 app.delete('/', (req, res) => {
-  
+    const index = alunos.findIndex(x => x.ra == req.query.ra);
+
+    if (index <= -1) {
+        return res.status(404).send('Aluno não encontrado');
+    }
+
+    alunos.splice(index, 1);
+    res.json(alunos);
+});
+
+app.delete('/:ra', (req, res) => {
+    let ra = req.params.ra;
+
+    index = alunos.findIndex(x => x.ra == ra);
+    if (index < 0) {
+        return res.status(404).send('Aluno não encontrado');
+    }
+
+    delete alunos[index].turma;
+    res.status(200).send('Turma deletada do aluno com sucesso');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+    console.log(`App rodando em: ${port}`)
 });
